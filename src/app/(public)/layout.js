8318eda4d4a8e43ab61fbf27da8bd8062edecc84
Ufuk ÇELEBİ <ufukcelebi0137@gmail.com>
2026-08-15 17:17:02 +0300

    fix: correct meta pixel script syntax for next.js

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -34,20 +34,24 @@ export default function PublicLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.8.0/vanilla-tilt.min.js" strategy="afterInteractive" />
         <Script src="https://unpkg.com/@phosphor-icons/web" strategy="afterInteractive" />
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {\`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1696269471434036');
-            fbq('track', 'PageView');
-          \`}
-        </Script>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1696269471434036');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className="dark-theme">
         <noscript>
