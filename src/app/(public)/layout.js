e13d93570d7f7454c94425bc76d5b9c3f5397828
Ufuk ÇELEBİ <ufukcelebi0137@gmail.com>
2026-08-16 00:57:15 +0300

    fix: process timeline layout, upgrade Inter to variable font with optical sizing

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -31,7 +31,7 @@ export default function PublicLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.8.0/vanilla-tilt.min.js" strategy="afterInteractive" />
         <Script src="https://unpkg.com/@phosphor-icons/web" strategy="afterInteractive" />
         <Script
